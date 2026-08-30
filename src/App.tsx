d18eb3d70a1d0f1b296a271c2a9b71ff9bdf0bd4
Alexandre Rego <alexfap.py@gmail.com>
2026-08-29 21:30:31 -0300

    Ajustando URL da API de Produção

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ export default function App() {
     e.preventDefault();
     setLoading(true);
     try {
-      const res = await fetch("https://sua-api.com/api/ocorrencia", {
+      const res = await fetch("https://grupoprima-backend-api.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
